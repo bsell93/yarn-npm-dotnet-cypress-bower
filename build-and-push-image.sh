46803e0d6679cb2c2b24b996ae520b3ee1436cf7
bsell93 <bryant.sell@edusource.us>
2018-05-31 15:03:54 -0400

    YNB-0000: Upgrade to dotnet core v2.1

--- build-and-push-image.sh
+++ build-and-push-image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build --no-cache -t bsell/yarn-npm-dotnet-cypress-bower:8-2.0.4 .
-docker push bsell/yarn-npm-dotnet-cypress-bower:8-2.0.4
+docker build --no-cache -t bsell/yarn-npm-dotnet-cypress-bower:8-2.1 .
+docker push bsell/yarn-npm-dotnet-cypress-bower:8-2.1
